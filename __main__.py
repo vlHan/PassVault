ed0fe35e3df9f9b87c0cd20896d997d122c559d7
vlHan <vitorleehan123@gmail.com>
2022-01-18 19:07:15 -0300

    Main file optimized

--- __main__.py
+++ __main__.py
@@ -25,61 +25,60 @@ class Manager:
             Menu(self.master_pw).menu_interface()
             self.run()
 
-        else:   
-            if os.path.isfile('vault.db'):
-                with sqlite3.connect('vault.db') as db: 
-                    cursor = db.cursor()
-                cursor.execute("SELECT * FROM masterpassword")
+        if os.path.isfile('vault.db'):
+            with sqlite3.connect('vault.db') as db: 
+                cursor = db.cursor()
+            cursor.execute("SELECT * FROM masterpassword")
 
-                for row in cursor.fetchall():
-                    master = row[0]
-                    salt = row[1] 
+            for row in cursor.fetchall():
+                master = row[0]
+                salt = row[1] 
 
-                self.master_pw = getpass.getpass('Enter your master password: ')
-                h = hmac.new(self.master_pw.encode(), msg=str(salt).encode(), digestmod=hashlib.sha3_512).hexdigest()
-                
-                if h == master:
-                    Menu(self.master_pw).menu_interface()
-                    self.run()
-                else:
-                    print(Fore.RED + 'The master password is not correct.\033[0m')
-                    self.main()
-                    
+            self.master_pw = getpass.getpass('Enter your master password: ')
+            h = hmac.new(self.master_pw.encode(), msg=str(salt).encode(), digestmod=hashlib.sha3_512).hexdigest()
+
+            if h == master:
+                Menu(self.master_pw).menu_interface()
+                self.run()
             else:
-                with sqlite3.connect('vault.db') as db: 
-                    cursor = db.cursor()
+                print(Fore.RED + 'The master password is not correct.\033[0m')
+                self.main()
 
-                print(Fore.GREEN + 'To start, you have to create a master password. Be careful not to lose it as it is unrecoverable.' + Style.RESET_ALL)
-                master_pw = getpass.getpass('Create a master password to use the program: ')
-                verify_master = getpass.getpass('Enter your master password again to verify it: ')
-                time.sleep(1)
-                
-                if master_pw == verify_master:
-                    if master_pw.isnumeric() or master_pw.isspace():
-                        print(Fore.RED + '\nThe password is not correct. Please try again.' + Style.RESET_ALL)
-                        return self.main()
+        else:
+            with sqlite3.connect('vault.db') as db: 
+                cursor = db.cursor()
 
-                    elif len(master_pw) < 8:
-                        print(Fore.RED + '\nThe password must have at least 8 caracters.' + Style.RESET_ALL)
-                        return self.main()
+            print(Fore.GREEN + 'To start, you have to create a master password. Be careful not to lose it as it is unrecoverable.' + Style.RESET_ALL)
+            master_pw = getpass.getpass('Create a master password to use the program: ')
+            verify_master = getpass.getpass('Enter your master password again to verify it: ')
+            time.sleep(1)
 
-                    else:
-                        with sqlite3.connect('vault.db') as db: 
-                            cursor = db.cursor()
-                            cursor.execute("CREATE TABLE masterpassword (password TEXT NOT NULL, salt TEXT NOT NULL);")
+            if master_pw == verify_master:
+                if master_pw.isnumeric() or master_pw.isspace():
+                    print(Fore.RED + '\nThe password is not correct. Please try again.' + Style.RESET_ALL)
+                    return self.main()
 
-                        salt = "".join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(32))
-                        master = hmac.new(master_pw.encode(), msg=str(salt).encode(), digestmod=hashlib.sha3_512).hexdigest()
-                        
-                        cursor.execute(f"INSERT INTO masterpassword VALUES('{master}', '{salt}')")
-                        db.commit()
-                        
-                        sys.exit(
-                            Fore.GREEN + "\nThank you! Restart the program and enter your master password to begin." + Style.RESET_ALL)
+                elif len(master_pw) < 8:
+                    print(Fore.RED + '\nThe password must have at least 8 caracters.' + Style.RESET_ALL)
+                    return self.main()
 
                 else:
-                    print(Fore.RED + '\nPassword do not match. Please try again.' + Style.RESET_ALL)
-                    return self.main()
+                    with sqlite3.connect('vault.db') as db: 
+                        cursor = db.cursor()
+                        cursor.execute("CREATE TABLE masterpassword (password TEXT NOT NULL, salt TEXT NOT NULL);")
+
+                    salt = "".join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(32))
+                    master = hmac.new(master_pw.encode(), msg=str(salt).encode(), digestmod=hashlib.sha3_512).hexdigest()
+
+                    cursor.execute(f"INSERT INTO masterpassword VALUES('{master}', '{salt}')")
+                    db.commit()
+
+                    sys.exit(
+                        Fore.GREEN + "\nThank you! Restart the program and enter your master password to begin." + Style.RESET_ALL)
+
+            else:
+                print(Fore.RED + '\nPassword do not match. Please try again.' + Style.RESET_ALL)
+                return self.main()
 
     def run(self) -> None:
         """Declarate main() and runtime
