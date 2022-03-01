e2b94322f698e1c7f575b450405636c157c7bb2b
carvalinho <60493469+carvalinh0@users.noreply.github.com>
2022-03-01 20:29:12 -0300

    Update encryption.py
    the criptografy mode was changed to AES_GCM

--- modules/encryption.py
+++ modules/encryption.py
@@ -2,7 +2,6 @@ from modules import *
 
 from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 
 
 class Encryption:
@@ -21,34 +20,32 @@ class Encryption:
             key {str} -- a master password to encrypt and decrypt
 
         Return 
-            {str} initial value and the cyphertext in base64 (concatenate string)
+            {str} tag, nonce and the cyphertext in base64 (concatenate string)
         """
 
         pssw = pssw.encode(self.encoding)
         key = key.encode(self.encoding)
-        cipher = AES.new(key, AES.MODE_CBC)
-        concatenate_bytes = cipher.encrypt(pad(pssw, AES.block_size))
+        cipher = AES.new(key, AES.MODE_GCM)
+        ciphertext, tag = cipher.encrypt_and_digest(pssw)
 
-        iv = b64encode(cipher.iv).decode(self.encoding)
-        concatenate = b64encode(concatenate_bytes).decode(self.encoding)
+        return b64encode(tag).decode('utf-8'), b64encode(cipher.nonce).decode('utf-8'), b64encode(ciphertext).decode('utf-8')
 
-        return (iv, concatenate)
-
-    def decrypt(self, initial_value: str, ciphertext: str, key: str) -> str:
+    def decrypt(self, tag: str, nonce: str, ciphertext: str, key: str) -> str:
         """
         Decrypt data using master password as the key.
 
         Arguments
-            initial_value {str} -- the value to pass to base64
+            tag {str} -- the value to decrypt the password
+            nonce {str} -- the value to decrypt the password
             ciphertext {str} -- to encrypt and decrypt
             key {str} -- a master password to encrypt and decrypt
 
         Returns 
             {str} The cypher text decrypted      
         """
-        initial_value = b64decode(initial_value)
-        concatenate = b64decode(ciphertext)
+        tag = b64decode(tag)
+        ciphertext = b64decode(ciphertext)
         key = key.encode(self.encoding)
-        cipher = AES.new(key, AES.MODE_CBC, initial_value)
-
-        return unpad(cipher.decrypt(concatenate), AES.block_size)
+        cipher = AES.new(key, AES.MODE_GCM, nonce=b64decode(nonce))
+        decoded = cipher.decrypt_and_verify(ciphertext, tag)
+        return decoded.decode()
